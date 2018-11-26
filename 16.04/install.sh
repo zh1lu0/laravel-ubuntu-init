33f3bfcdbadeaede6cf5b90ebaebb51bfea6f720
Zhi <luoziseaer@gmail.com>
2018-11-26 11:34:37 +0800

    add mysql client

--- 16.04/install.sh
+++ 16.04/install.sh
@@ -61,7 +61,7 @@ function install_others {
     apt-get remove -y apache2
     debconf-set-selections <<< "mysql-server mysql-server/root_password password ${MYSQL_ROOT_PASSWORD}"
     debconf-set-selections <<< "mysql-server mysql-server/root_password_again password ${MYSQL_ROOT_PASSWORD}"
-    apt-get install -y nginx mysql-server redis-server memcached beanstalkd sqlite3
+    apt-get install -y nginx mysql-server mysql-client redis-server memcached beanstalkd sqlite3
     chown -R ${WWW_USER}.${WWW_USER_GROUP} /var/www/
     systemctl enable nginx.service
 }
